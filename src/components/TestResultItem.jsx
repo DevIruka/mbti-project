8e58be9d431d3ab1197330a85a463664af8ec99c
minjun <qkralswns1998@gmail.com>
2024-11-27 08:56:53 +0900

    fix result 화면 버그 해결.

--- src/components/TestResultItem.jsx
+++ src/components/TestResultItem.jsx
@@ -1,8 +1,7 @@
 import { mbtiDescriptions } from "../utils/mbtiCalculator";
 import { getUserProfile } from "../api/auth";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { StBlueButton, StRedButton } from "../styles/globalStyle/Stbutton";
-import { deleteTestResult, updateTestResultVisibility } from "../api/json";
 import defaultImg from "/guest.png";
 
 const TestResultItem = ({
@@ -17,7 +16,7 @@ const TestResultItem = ({
 }) => {
   const token = localStorage.getItem("accessToken");
 
-  const { data: userProfile } = useQuery({
+  const { data: userProfile, isPending } = useQuery({
     queryKey: ["userProfile"],
     queryFn: async () => {
       const data = await getUserProfile(token);
@@ -35,31 +34,37 @@ const TestResultItem = ({
     const response = await deleteMutation(jsonId);
   };
 
-  return (
-    <li className="flex flex-col items-center shadow-lg m-10 bg-white rounded min-h-[300px] w-[500px]">
-      <span>{nickname}</span>
-      <img
-        src={avatar || defaultImg}
-        className="h-[50px]"
-        alt="유저 프로파일 이미지입니다."
-      />
-      {userProfile?.id === cardId && (
-        <div className="flex flex-row gap-5 p-3">
-          {visibility ? (
-            <StBlueButton onClick={visibilityBtnHandler}>
-              비공개로 전환
-            </StBlueButton>
-          ) : (
-            <StBlueButton onClick={visibilityBtnHandler}>
-              공개로 전환
-            </StBlueButton>
-          )}
-          <StRedButton onClick={deleteBtnHandler}>Delete</StRedButton>
-        </div>
-      )}
-      <span>{mbtiResult}</span>
-      <p className="text-sm">{description}</p>
-    </li>
-  );
+  if (isPending) {
+    <h2>로딩 중입니다. 조금만 기다려주세요.</h2>;
+  }
+
+  if (visibility === true || userProfile?.id === cardId) {
+    return (
+      <li className="flex flex-col items-center shadow-lg m-10 bg-white rounded min-h-[300px] w-[500px]">
+        <span>{nickname}</span>
+        <img
+          src={avatar || defaultImg}
+          className="h-[50px]"
+          alt="유저 프로파일 이미지입니다."
+        />
+        {userProfile?.id === cardId && (
+          <div className="flex flex-row gap-5 p-3">
+            {visibility ? (
+              <StBlueButton onClick={visibilityBtnHandler}>
+                비공개로 전환
+              </StBlueButton>
+            ) : (
+              <StBlueButton onClick={visibilityBtnHandler}>
+                공개로 전환
+              </StBlueButton>
+            )}
+            <StRedButton onClick={deleteBtnHandler}>Delete</StRedButton>
+          </div>
+        )}
+        <span>{mbtiResult}</span>
+        <p className="text-sm">{description}</p>
+      </li>
+    );
+  }
 };
 export default TestResultItem;
