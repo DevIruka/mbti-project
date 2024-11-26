70184438eb20750e36bfe36b2e86dba9559d2569
minjun <qkralswns1998@gmail.com>
2024-11-26 12:46:55 +0900

    feat test 기능 추가? 결과가 고정된 상태로 출력되는 문제 있음.

--- src/api/json.js
+++ src/api/json.js
@@ -1,6 +1,19 @@
 import axios from "axios";
 
 const jsonApi = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "http://localhost:5000",
 });
 
+export const getTestResults = async () => {
+  const response = await jsonApi.get();
+  return response.data;
+};
+
+export const createTestResult = async (resultData) => {
+  const response = await jsonApi.post("/testResults", resultData);
+  return response.data;
+};
+
+export const deleteTestResult = async (id) => {};
+
+export const updateTestResultVisibility = async (id, visibility) => {};
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { getUserProfile, updateProfile } from "../api/auth";
 import { useQuery } from "@tanstack/react-query";
 import StMain from "../styles/globalStyle/StMain";
+import Container from "../styles/globalStyle/Container";
 
 const Profile = () => {
   const token = localStorage.getItem("accessToken");
@@ -38,36 +39,47 @@ const Profile = () => {
 
   return (
     <StMain>
-      <h1>프로필 수정</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="flex flex-col items-center">
-          <span>{userProfile?.nickname}</span>
-          {userProfile.avatar ? (
-            <img src={userProfile.avatar} alt="유저 프로필 이미지입니다."
-            className="h-[150px]" />
-          ) : (
-            <img
-              src="/guest.png"
-              alt="유저 프로필 이미지입니다."
-              className="h-[100px]"
+      <Container>
+        <p className="text-2xl">프로필 수정</p>
+        <form onSubmit={handleSubmit}>
+          <div className="flex flex-col items-center">
+            <span>{userProfile?.nickname}님 어서오세요.</span>
+            {userProfile.avatar ? (
+              <img
+                src={userProfile.avatar}
+                alt="유저 프로필 이미지입니다."
+                className="h-[150px]"
+              />
+            ) : (
+              <img
+                src="/guest.png"
+                alt="유저 프로필 이미지입니다."
+                className="h-[100px]"
+              />
+            )}
+            <input
+              name="nickname"
+              className="border border-gray-300 rounded px-2 py-1 w-[300px] m-2"
+              onChange={textHandler}
             />
-          )}
-          <input
-            name="nickname"
-            className="border border-gray-300 rounded px-2 py-1 w-[300px] m-2"
-            onChange={textHandler}
-          />
-        </div>
-        <div className="flex flex-col">
-          <input
-            type="file"
-            accept="image/*"
-            onChange={fileHandler}
-            name="imgFile"
-          />
-          <button type="submit">프로필 업데이트</button>
-        </div>
-      </form>
+          </div>
+          <div className="flex flex-col">
+            <input
+              type="file"
+              accept="image/*"
+              onChange={fileHandler}
+              name="imgFile"
+              className="p-3"
+            />
+            <button
+              className="px-4 py-2 bg-blue-500 text-white font-semibold rounded hover:bg-blue-600 transition duration-300"
+              type="submit"
+            >
+              프로필 업데이트
+            </button>
+          </div>
+        </form>
+      </Container>
     </StMain>
   );
 };
